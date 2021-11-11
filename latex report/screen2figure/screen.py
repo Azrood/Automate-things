d14ea5e7d81413bc4bb968bfa309c263edc74a77
Azrood <53349565+Azrood@users.noreply.github.com>
2021-11-11 15:47:07 +0000

    Update screen.py

--- latex report/screen2figure/screen.py
+++ latex report/screen2figure/screen.py
@@ -11,7 +11,9 @@ template = r""" \begin{figure}[H]
 \end{figure}"""
 
 path = "." if len(sys.argv)==1 else sys.argv[1]
-files = [str(p) for p in Path(path).iterdir() if p.is_file() and str(p).rsplit(".", 1)[-1].lower() in ("png", "jpg", "jpeg", "eps", "bmp")]
+files = [os.path.basename(str(p)) 
+         for p in Path(path).iterdir() 
+         if p.is_file() and str(p).rsplit(".", 1)[-1].lower() in ("png", "jpg", "jpeg", "eps", "bmp")]
 files.sort(key=os.path.getmtime)
 for p in files:
     print(template.replace("$FILENAME", p).replace("$CAPTION", (r"\caption{"+str(p).rsplit(".",1)[0]+"}").replace("_"," ")
